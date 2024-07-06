7f073204260d7f2910548c7405946cc4309b25e2
Dmitry Andriushkov <115940434+DmitryAndriushkov@users.noreply.github.com>
2024-07-07 02:42:45 +0300

    Added my second task of the first lection

---
+++ Lesson_1/Task_2/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    double num1, num2, sum;
+
+    cout << "Hello, this program can add up two numbers\nEnter your first number: ";
+    cin >> num1;
+
+    cout << "Enter your second number: ";
+    cin >> num2;
+
+    sum = num1 + num2;
+
+    cout << "Result: " << sum;
+}
